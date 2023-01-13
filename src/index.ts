00fa0fb79d64a57676b457b1c4b28eb173e48162
nikolaischunk <58025175+nikolaischunk@users.noreply.github.com>
2023-01-13 15:31:14 +0100

    Added template files

---
+++ src/index.ts
@@ -0,0 +1,30 @@
+import './lib/setup';
+import { LogLevel, SapphireClient } from '@sapphire/framework';
+import { GatewayIntentBits, Partials } from 'discord.js';
+
+const client = new SapphireClient({
+	defaultPrefix: 'b!',
+	intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages],
+	regexPrefix: /^(hey +)?bot[,! ]/i,
+	caseInsensitiveCommands: true,
+	logger: {
+		level: LogLevel.Debug
+	},
+	shards: 'auto',
+	partials: [Partials.Channel],
+	loadMessageCommandListeners: true
+});
+
+const main = async () => {
+	try {
+		client.logger.info('Logging in');
+		await client.login();
+		client.logger.info('logged in');
+	} catch (error) {
+		client.logger.fatal(error);
+		client.destroy();
+		process.exit(1);
+	}
+};
+
+main();
